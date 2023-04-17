ed77e68a041b07943d556a199516c6c5a3da0365
zachSelindh <zachselindh@gmail.com>
2023-04-17 16:18:52 -0500

    COnfigure services to return params from url

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,27 +10,31 @@ export class RecipeService {
 
     private recipes: Recipe[] = [
         new Recipe('Spaghet', 
-        `Hey I'm walkin' 
-        'ere`, 
-        'https://www.cookipedia.co.uk/wiki/images/b/bf/Rock_salmon_with_garlic_and_coriander.jpg',
-        [ 
-            new Ingredient('Salmon', 1),
-            new Ingredient('Pasta', 2),
-    ]),
+            `Hey I'm walkin' ere`, 
+            'https://www.cookipedia.co.uk/wiki/images/b/bf/Rock_salmon_with_garlic_and_coriander.jpg',
+            [ 
+                new Ingredient('Salmon', 1),
+                new Ingredient('Pasta', 2),
+            ]
+        ),
         new Recipe('Beesechurger', 
-        `Huh`, 
-        `https://www.austinchronicle.com/binary/8e59/food_JewBoyBurgers3.jpg`,
-        [ 
-            new Ingredient('Beef', 15),
-            new Ingredient('Bread', 2),
-            new Ingredient('Cheese', 1),
-    ]),
-      ];     
+            `Huh`, 
+            `https://www.austinchronicle.com/binary/8e59/food_JewBoyBurgers3.jpg`,
+            [ 
+                new Ingredient('Beef', 15),
+                new Ingredient('Bread', 2),
+                new Ingredient('Cheese', 1),
+        ]),
+    ];     
 
     constructor(private slService: ShoppingListService) {}
 
     getRecipes() {
         return [...this.recipes];
+    }
+
+    getRecipe(index: number) {
+        return this.recipes[index];
     }
 
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
